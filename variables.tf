efb90b5c88a2c998bb6502a3afdb1a1988564101
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-05-31 21:23:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "aws_provider_region" {
+  type    = string
+  default = <INSERT REGION>
+}
 
+variable "aws_provider_access_key" {
+  type    = string
+  default = <INSERT ACCESS KEY>
+}
+
+variable "aws_provider_secret_key" {
+  type    = string
+  default = <INSERT SECRET KEY>
+}
