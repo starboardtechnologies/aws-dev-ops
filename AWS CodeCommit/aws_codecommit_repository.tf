f67866fdbfa7d9eff377ab4092f53cd7124706ad
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-01 01:31:27 -0500

    Update aws_codecommit_repository.tf

--- AWS CodeCommit/aws_codecommit_repository.tf
+++ AWS CodeCommit/aws_codecommit_repository.tf
@@ -2,3 +2,11 @@ resource "aws_codecommit_repository" "aws_codecommit_repository_sns_trigger_emai
   repository_name = var.aws_codecommit_repository_sns_trigger_email_notification
   description     = "AWS CodeCommit Repository with SNS Trigger(s) enabled to send email notifications when any changes are made to repository."
 }
+
+output "arn" {
+  value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
+}
+
+output "repository_id" {
+  value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
+}
