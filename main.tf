52d75a1654b0dd6497d9c3e6816f2e35452d1c87
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-05-31 21:22:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region      = var.aws_provider_region
+  access_key  = var.aws_provider_access_key
+  secret_key  = var.aws_provider_secret_key
+}
