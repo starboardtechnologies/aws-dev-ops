3c4adfb709a5c111e27731463d6658abf6bc4393
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-03 15:17:36 -0500

    Delete aws_codecommit_repository.tf

--- CICD/AWS CodeCommit/aws_codecommit_repository.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_codecommit_repository" "aws_codecommit_repository_sns_trigger_email_notification" {
-  repository_name = var.aws_codecommit_repository_sns_trigger_email_notification
-  description     = "AWS CodeCommit Repository with SNS Trigger(s) enabled to send email notifications when any changes are made to repository."
-}
