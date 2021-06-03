4dd04add3dd4270c451d0912773a0eda07cded08
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-03 15:13:22 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,13 @@
+##########################################################################################
 
+AWS CODECOMMIT
+
+output "arn" {
+  value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
+}
+
+output "repository_id" {
+  value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
+}
+
+##########################################################################################
