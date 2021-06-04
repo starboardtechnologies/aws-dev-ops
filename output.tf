e7ce722122cc2bfba833e1e91d033ca92095b3d3
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-04 14:42:30 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,24 @@
 ##########################################################################################
 
+AWS CODEBUILD
+
+output "aws-codebuild-input-arn" {
+    value = aws_s3_bucket.aws_s3_bucket_codebuild_input
+}
+
+output "aws-codebuild-input-s3-bucket-id" {
+    value = aws_s3_bucket.aws_s3_bucket_codebuild_input
+}
+
+##########################################################################################
+
 AWS CODECOMMIT
 
-output "codecommit-repository-arn" {
+output "aws-codecommit-repository-arn" {
   value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
 }
 
-output "codecommit-repository-id" {
+output "aws-codecommit-repository-id" {
   value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
 }
 
