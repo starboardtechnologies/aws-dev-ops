2d9790d5c68ab79db20dff80c215769368f91c58
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-04 14:37:29 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,11 +2,11 @@
 
 AWS CODECOMMIT
 
-output "arn" {
+output "arn-codecommit-repository" {
   value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
 }
 
-output "repository_id" {
+output "codecommit-repository_id" {
   value = aws_codecommit_repository.aws_codecommit_repository_sns_trigger_email_notification
 }
 
