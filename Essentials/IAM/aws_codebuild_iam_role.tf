329f4fd88091b313703ff2a3caea302176963f60
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-04 14:18:29 -0500

    Update and rename aws_iam_role.tf to aws_codebuild_iam_role.tf

---
+++ Essentials/IAM/aws_codebuild_iam_role.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "aws-codebuild-iam-role" {
+  name = "aws-codebuild-iam-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
