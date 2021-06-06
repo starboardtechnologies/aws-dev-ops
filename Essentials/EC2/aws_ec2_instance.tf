24a872885c7708604771893178ae55e6b6761c85
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-06 18:45:02 -0500

    Update aws_ec2_instance.tf

--- Essentials/EC2/aws_ec2_instance.tf
+++ Essentials/EC2/aws_ec2_instance.tf
@@ -1,1 +1,19 @@
+resource "aws_instance" "ec2instance" {
+  count = var.instance_count
+  ami           = var.ec2_instance_ami
+  instance_type = var.ec2_instance_type
+  tags = {
+      Name = element(var.instance_tags, count.index)
+  }
+}
+
+variable "instance_count" {
+  type = number
+  default = 3
+}
+
+variable "instance_tags" {
+  type = list
+  default = ["dev-ec2instance", "stage-ec2instance", "prod-ec2instance"]
+}
 
