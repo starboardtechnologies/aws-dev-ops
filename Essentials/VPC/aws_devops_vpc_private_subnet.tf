ea989fde5fbc00c1ae166638532becf8c9172dad
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-05 20:24:56 -0500

    Update aws_devops_vpc_private_subnet.tf

--- Essentials/VPC/aws_devops_vpc_private_subnet.tf
+++ Essentials/VPC/aws_devops_vpc_private_subnet.tf
@@ -2,8 +2,6 @@ resource "aws_subnet" "aws-devops-vpc-private-subnet" {
   vpc_id     = aws_vpc.devops.id
   cidr_block = "10.0.0.0/24"
 
-  map_customer_owned_ip_on_launch = false
-
   tags = {
     Name = "aws-vpc=devops-private-subnet"
   }
