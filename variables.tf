5519ca0ee0e8919c3d2a9cc51abf561958d9d48b
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-05 20:27:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+############################################################################################
+
 variable "aws_provider_region" {
   type    = string
   default = <INSERT REGION>
@@ -13,7 +15,11 @@ variable "aws_provider_secret_key" {
   default = <INSERT SECRET KEY>
 }
 
+############################################################################################
+
 variable "aws_codecommit_repository_sns_trigger_email_notification_name" {
   type    = string
   default = <INSERT REPOSITORY NAME>
 }
+
+############################################################################################
