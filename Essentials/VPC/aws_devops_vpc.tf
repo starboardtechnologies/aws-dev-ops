1575ce76bda7f304c4b71dc0d1f6ed30cffa5966
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-06 21:18:15 -0500

    Update aws_devops_vpc.tf

--- Essentials/VPC/aws_devops_vpc.tf
+++ Essentials/VPC/aws_devops_vpc.tf
@@ -1,6 +1,17 @@
 resource "aws_vpc" "devops" {
+  count = var.vpc_count
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "devops"
+      Name = element(var.vpc_tags, count.index)
   }
+}
+
+variable "vpc_count" {
+  type = number
+  default = 3
+}
+
+variable "vpc_tags" {
+  type = list
+  default = ["dev-vpc", "stage-vpc", "prod-vpc"]
 }
