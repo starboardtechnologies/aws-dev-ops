00e71e1e86a8dc0b9a7837f4cf31c50adb934da1
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-10 14:36:03 -0500

    Update aws_ec2_instance.tf

--- Essentials/EC2/aws_ec2_instance.tf
+++ Essentials/EC2/aws_ec2_instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2instance" {
-  count = var.instance_count
-  ami           =  var.ec2_instance_ami
-  instance_type =  var.ec2_instance_type
+  ami               =  var.ec2_instance_ami
+  availability_zone = var.ec2_instance_availability_zone
+  count             = var.instance_count
+  instance_type     =  var.ec2_instance_type
   tags = {
       Name = element(var.instance_tags, count.index)
   }
