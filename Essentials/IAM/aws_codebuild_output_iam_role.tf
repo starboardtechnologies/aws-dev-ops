ef75706a098ba514b3e6b028f08de0ee94624c91
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-11 10:55:55 -0500

    Create aws_codebuild_output_iam_role.tf

---
+++ Essentials/IAM/aws_codebuild_output_iam_role.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "aws-s3-bucket-codebuild-output-iam-role" {
+  count = 3  
+  name  = var.aws_s3-bucket_codebuild_output_iam_role[count.index]
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
