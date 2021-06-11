9e61692c33738153bef4b627e5650105aac679c2
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-11 10:13:37 -0500

    Update aws_s3_bucket_codebuild_input.tf

--- Essentials/S3/aws_s3_bucket_codebuild_input.tf
+++ Essentials/S3/aws_s3_bucket_codebuild_input.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "aws_s3_bucket_codebuild_input" {
-  bucket = var.aws_s3_bucket_codebuild_input_name
+  count = 3  
+  bucket = var.aws_s3_bucket_codebuild_input_names[count.index]
   acl    = "private"
 }
