e8a54f26ec2444ae29593d0fa9d1aae004e16189
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-11 03:11:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,18 +3,18 @@
 AWS PROVIDER
 
 variable "aws_provider_region" {
-  type    = string
-  default = <"INSERT REGION">
+  type      = string
+  default   = <"INSERT REGION">
 }
 
 variable "aws_provider_access_key" {
-  type    = string
-  default = <"INSERT ACCESS KEY">
+  type      = string
+  default   = <"INSERT ACCESS KEY">
 }
 
 variable "aws_provider_secret_key" {
-  type    = string
-  default = <"INSERT SECRET KEY">
+  type      = string
+  default   = <"INSERT SECRET KEY">
 }
 
 ############################################################################################
@@ -22,8 +22,8 @@ variable "aws_provider_secret_key" {
 AWS CODECOMMIT
 
 variable "aws_codecommit_repository_sns_trigger_email_notification_name" {
-  type    = string
-  default = <"INSERT REPOSITORY NAME">
+  type      = string
+  default   = <"INSERT REPOSITORY NAME">
 }
 
 ############################################################################################
