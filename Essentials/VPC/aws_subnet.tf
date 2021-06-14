5e4cdcbaaa6c5a4015f17af30c4262e8d3b66c2f
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-14 17:29:05 -0500

    Update aws_subnet.tf

--- Essentials/VPC/aws_subnet.tf
+++ Essentials/VPC/aws_subnet.tf
@@ -2,5 +2,8 @@ resource "aws_subnet" "vpc_subnets" {
   count      = 3  
   cidr_block = var.vpc_cidr_blocks[count.index]
   vpc_id     = var.vpc_id[count.index]
+  tags = {
+      Name = element(var.vpc_subnet_tags, count.index)
+  }
 }
 
