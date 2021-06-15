1db3dc4b5680f8bace14fd4164aaa91a65e4806c
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-15 16:59:46 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -107,11 +107,6 @@ variable "vpc_tags" {
   default = [<"INSERT DEV VPC NAME">, <"INSERT STAGE VPC NAME">, <"INSERT PROD VPC NAME">]
 }
 
-variable "vpc_cidr_blocks" {
-    type    = list
-    default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-}
-
 variable "vpc_id" {
   type    = list 
   default = [<INSERT DEV VPC ID>, <INSERT STAGE VPC ID>, <INSERT PROD VPC ID>]
