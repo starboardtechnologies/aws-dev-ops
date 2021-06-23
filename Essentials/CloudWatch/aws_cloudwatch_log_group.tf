90722d531ac663b226822e269a560791627072ad
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-23 14:35:43 -0500

    Delete Essentials directory

--- Essentials/CloudWatch/aws_cloudwatch_log_group.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_cloudwatch_log_group" "aws-devops-vpc-flow-log-cloudwatch-log-group" {
-  name = "aws-devops-vpc-flow-log-cloudwatch-log-group"
-}
--- Essentials/VPC/aws_devops_vpc_private_subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "aws-devops-vpc-private-subnet" {
-  vpc_id     = aws_vpc.devops.id
-  cidr_block = "10.0.0.0/24"
-
-  tags = {
-    Name = "aws-vpc=devops-private-subnet"
-  }
-}
