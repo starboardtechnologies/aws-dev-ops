11bb9a7e7ea5038311edef8eea109a4b6b9cbcab
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-06-26 22:02:25 -0500

    Update aws_athena_database.tf

--- DevOps/Athena/aws_athena_database.tf
+++ DevOps/Athena/aws_athena_database.tf
@@ -1,5 +1,5 @@
 resource "aws_athena_database" "devops_athena-databases" {
   count  = 3  
   name   = var.devops_athena_database_names[count.index]
-  bucket = var.devopsathenadatabases3buckets[count.index]
+  bucket = var.devops_athena_database_s3_buckets[count.index]
 }
