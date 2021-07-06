6212bbba6800a3e7aa6afb711798a0ebdd69ee3d
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-07-06 15:36:51 -0500

    Update aws_autoscaling_group.tf

--- Essentials/Autoscaling/aws_autoscaling_group.tf
+++ Essentials/Autoscaling/aws_autoscaling_group.tf
@@ -1,15 +1,15 @@
 resource "aws_autoscaling_group" "devops_autoscaling_groups" {
-  count                     = 3  
-  name                      = var.devops_autoscaling_group_names[count.index]
-  max_size                  = var.devops_autoscaling_groups_max_size[count.index]
-  min_size                  = var.devops_autoscaling_groups_min_size[count.index]
-  health_check_grace_period = var.devops_autoscaling_group_health_check_grace_period[count.index]
-  health_check_type         = var.devops_autoscaling_group_health_check_type[count.index]
-  desired_capacity          = var.devops_autoscaling_group_desired_capacity[count.index]
-  force_delete              = var.devops_autoscaling_group_force_delete[count.index]
-  placement_group           = var.devops_ec2_placement_group_names[count.index]
-  launch_configuration       = var.devops_launch_configuration_names[count.index]
-  vpc_zone_identifier        = var.vpc_zone_identifers
+  count                       = 3  
+  name                        = var.devops_autoscaling_group_names[count.index]
+  max_size                    = var.devops_autoscaling_groups_max_size[count.index]
+  min_size                    = var.devops_autoscaling_groups_min_size[count.index]
+  health_check_grace_period   = var.devops_autoscaling_group_health_check_grace_period[count.index]
+  health_check_type           = var.devops_autoscaling_group_health_check_type[count.index]
+  desired_capacity            = var.devops_autoscaling_group_desired_capacity[count.index]
+  force_delete                = var.devops_autoscaling_group_force_delete[count.index]
+  placement_group             = var.devops_ec2_placement_group_names[count.index]
+  launch_configuration        = var.devops_launch_configuration_names[count.index]
+  vpc_zone_identifier         = var.vpc_zone_identifers
 
 
   tag {
