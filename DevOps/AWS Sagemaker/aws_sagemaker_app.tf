c8b8b3659d80cc58283ac13d49690717a9273261
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-08-09 20:02:01 -0500

    Create aws_sagemaker_app.tf

---
+++ DevOps/AWS Sagemaker/aws_sagemaker_app.tf
@@ -0,0 +1,11 @@
+resource "aws_sagemaker_app" "devops_aws_sagemaker_apps" {
+  count             = 3
+  domain_id         = var.aws_sagemaker_domain_id
+  user_profile_name  = var.aws_sagemaker_user_profile_name
+  app_name          = var.aws_sagemaker_app_name
+  app_type          = "JupyterServer"
+
+  tags = {
+        Name = element(var.devops_aws_sagemaker_apps_tags, count.index)
+    }
+}
