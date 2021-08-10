7825d480cf3f12abcc0ca407d12b4e52b9016f9e
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-08-09 20:02:20 -0500

    Update aws_sagemaker_app.tf

--- DevOps/AWS Sagemaker/aws_sagemaker_app.tf
+++ DevOps/AWS Sagemaker/aws_sagemaker_app.tf
@@ -1,9 +1,9 @@
 resource "aws_sagemaker_app" "devops_aws_sagemaker_apps" {
-  count             = 3
-  domain_id         = var.aws_sagemaker_domain_id
+  count              = 3
+  domain_id          = var.aws_sagemaker_domain_id
   user_profile_name  = var.aws_sagemaker_user_profile_name
-  app_name          = var.aws_sagemaker_app_name
-  app_type          = "JupyterServer"
+  app_name           = var.aws_sagemaker_app_name
+  app_type           = "JupyterServer"
 
   tags = {
         Name = element(var.devops_aws_sagemaker_apps_tags, count.index)
