afa18d91fbeab0d5b0a572e5b9d814ad916263df
starboardtechnologies <40809170+starboardtechnologies@users.noreply.github.com>
2021-11-11 20:50:27 -0600

    Create aws_shield_protection.tf

---
+++ DevOps/Security/AWS Shield/aws_shield_protection.tf
@@ -0,0 +1,8 @@
+resource "aws_shield_protection" "example" {
+  name         = "example"
+  resource_arn = "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:eip-allocation/${aws_eip.example.id}"
+
+  tags = {
+    Environment = "Dev"
+  }
+}
